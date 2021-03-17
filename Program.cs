99cf91a04bb2dd1a633788c5d6d4bd2fb2aa2bc1
avidsaputro <avidsaputro@students.amikom.ac.id>
2021-03-17 11:49:58 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tugastaxi
+{
+    static class Program
+    {
+        static void Main(string[]args)
+        {
+            taxi taxi = new taxi();
+            taxi.Drivername = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.taxiinfo();
+            taxi.pickuppassenger();
+            taxi.Dropoffpassenger();
+            Console.Read();
+        }
+
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugastaxi
+{
+    class taxi
+    {
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("Driver name    : {0}", Drivername);
+            Console.WriteLine("On Duty        : {0}", OnDuty);
+            Console.WriteLine("Num Passenger  : {0}", NumPassenger);
+        }
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang",Drivername);
+        }
+        public void Dropoffpassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang",Drivername);
+        }
+
+    }
+}
